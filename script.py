fd44b10da61fd115aa41f38f1e73b48ebe33f2b9
vibhor <vats.vibhor.07@gmail.com>
2020-02-09 19:40:40 +0530

    removed some code

--- script.py
+++ script.py
@@ -1,11 +1,9 @@
 import sys
 
-print(sys.version)
-print(sys.executable)
 
 
 def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
+    greeting = 'Hello how are you?, {}'.format(who_to_greet)
     return greeting
 
 
